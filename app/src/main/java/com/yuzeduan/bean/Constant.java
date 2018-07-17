0fd7088af46dd3ec44ed055e4a828da5f1fca210
Yuzeduan <yuzeduan99@163.com>
2018-07-17 13:58:59 +0800

    refactor(bean&app/build.gradle)封装了获取具体url的方法在常量类,以及更新sdk

--- app/src/main/java/com/yuzeduan/bean/Constant.java
+++ app/src/main/java/com/yuzeduan/bean/Constant.java
@@ -13,4 +13,70 @@ public class Constant {
     public static final String NEW_MUSICLIST_URL = "http://v3.wufazhuce.com:8000/api/channel/music/more/0?platform=android";
     public static final String NEW_MOVIELIST_URL = "http://v3.wufazhuce.com:8000/api/channel/movie/more/0?platform=android";
     public static final String NEW_INSETID_URL = "http://v3.wufazhuce.com:8000/api/hp/idlist/0?version=3.5.0&platform=android";
+    private static StringBuilder mBuilder = new StringBuilder();
+    /**
+     * 获取插画详细的URL
+     * @param id 获取插画的具体id
+     * @return 返回获取插画的URL
+     */
+    public static String createInsetUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/hp/detail/";
+        String tail = "?version=3.5.0&platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    /**
+     * 用于获取阅读列表URL
+     * @param id
+     * @return
+     */
+    public static String createReadListUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/channel/reading/more/";
+        String tail = "?channel=wdj&version=4.0.2&platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    public static String createReadingUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/essay/";
+        String tail = "?platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    public static String createMusicUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/music/detail/";
+        String tail = "?version=3.5.0&platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    public static String createMovieUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/movie/";
+        String tail = "/story/1/0?platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    public static String createReadingCommentUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/comment/praiseandtime/essay/";
+        String tail = "/0?&platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    public static String createMusicCommentUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = "http://v3.wufazhuce.com:8000/api/comment/praiseandtime/music/";
+        String tail = "/0?&platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
+
+    public static String createMovieCommentUrl(String id){
+        mBuilder.delete(0, mBuilder.length());
+        String head = " http://v3.wufazhuce.com:8000/api/comment/praiseandtime/movie/";
+        String tail = "/0?&platform=android";
+        return mBuilder.append(head).append(id).append(tail).toString();
+    }
 }
